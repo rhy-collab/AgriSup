66e61ff8947992cf27a302c7931af1c5f5d31f6a
Sonney Patel <sonney.patel@gmail.com>
2021-01-15 16:56:07 +0000

    AGRI - 01 : 1. Added Fonts 2. Added Navigational Controller 3. Added Colors 4. Renamed Assets

---
+++ AgriSup/Constants.swift
@@ -0,0 +1,18 @@
+//
+//  Constants.swift
+//  AgriSup
+//
+//  Created by Sonney Patel on 15/01/2021.
+//
+
+import Foundation
+
+
+struct K {
+    
+    struct Segues {
+        static let LOGIN_TO_MAIN_VIEW = "LoginToMainView"
+        static let REGISTER_TO_GET_STARTED = "RegisterToGetStarted"
+    }
+    
+}
