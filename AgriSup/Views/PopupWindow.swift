b87b7d334e14a7913ba4513e9a3e50f1e540650a
Sonney Patel <sonney.patel@gmail.com>
2021-01-18 15:37:22 +0000

    Error Handler Styling
    Rounded the corners and styled the error handler a little bit (WIP)

--- AgriSup/Views/PopupWindow.swift
+++ AgriSup/Views/PopupWindow.swift
@@ -23,79 +23,82 @@ private class PopUpWindowView: UIView {
         super.init(frame: CGRect.zero)
         
         backgroundColor = UIColor.black.withAlphaComponent(0.3)
-               
-               // Popup Background
-        popupView.backgroundColor = UIColor.blue
-           popupView.layer.borderWidth = BorderWidth
-           popupView.layer.masksToBounds = true
-           popupView.layer.borderColor = UIColor.white.cgColor
-           
-           // Popup Title
-           popupTitle.textColor = UIColor.white
-           popupTitle.backgroundColor = UIColor.yellow
-           popupTitle.layer.masksToBounds = true
-           popupTitle.adjustsFontSizeToFitWidth = true
-           popupTitle.clipsToBounds = true
-           popupTitle.font = UIFont.systemFont(ofSize: 23.0, weight: .bold)
-           popupTitle.numberOfLines = 1
-           popupTitle.textAlignment = .center
-           
-           // Popup Text
-           popupText.textColor = UIColor.white
-           popupText.font = UIFont.systemFont(ofSize: 16.0, weight: .semibold)
-           popupText.numberOfLines = 0
-           popupText.textAlignment = .center
-           
-           // Popup Button
-           popupButton.setTitleColor(UIColor.white, for: .normal)
-           popupButton.titleLabel?.font = UIFont.systemFont(ofSize: 23.0, weight: .bold)
-           popupButton.backgroundColor = UIColor.yellow
-           
-           popupView.addSubview(popupTitle)
-           popupView.addSubview(popupText)
-           popupView.addSubview(popupButton)
-           
-           // Add the popupView(box) in the PopUpWindowView (semi-transparent background)
-           addSubview(popupView)
-           
-           
-           // PopupView constraints
-           popupView.translatesAutoresizingMaskIntoConstraints = false
-           NSLayoutConstraint.activate([
-               popupView.widthAnchor.constraint(equalToConstant: 293),
-               popupView.centerYAnchor.constraint(equalTo: self.centerYAnchor),
-               popupView.centerXAnchor.constraint(equalTo: self.centerXAnchor)
-               ])
-           
-           // PopupTitle constraints
-           popupTitle.translatesAutoresizingMaskIntoConstraints = false
-           NSLayoutConstraint.activate([
-               popupTitle.leadingAnchor.constraint(equalTo: popupView.leadingAnchor, constant: BorderWidth),
-               popupTitle.trailingAnchor.constraint(equalTo: popupView.trailingAnchor, constant: -BorderWidth),
-               popupTitle.topAnchor.constraint(equalTo: popupView.topAnchor, constant: BorderWidth),
-               popupTitle.heightAnchor.constraint(equalToConstant: 55)
-               ])
-           
-           
-           // PopupText constraints
-           popupText.translatesAutoresizingMaskIntoConstraints = false
-           NSLayoutConstraint.activate([
-               popupText.heightAnchor.constraint(greaterThanOrEqualToConstant: 67),
-               popupText.topAnchor.constraint(equalTo: popupTitle.bottomAnchor, constant: 8),
-               popupText.leadingAnchor.constraint(equalTo: popupView.leadingAnchor, constant: 15),
-               popupText.trailingAnchor.constraint(equalTo: popupView.trailingAnchor, constant: -15),
-               popupText.bottomAnchor.constraint(equalTo: popupButton.topAnchor, constant: -8)
-               ])
-
-           
-           // PopupButton constraints
-           popupButton.translatesAutoresizingMaskIntoConstraints = false
-           NSLayoutConstraint.activate([
-               popupButton.heightAnchor.constraint(equalToConstant: 44),
-               popupButton.leadingAnchor.constraint(equalTo: popupView.leadingAnchor, constant: BorderWidth),
-               popupButton.trailingAnchor.constraint(equalTo: popupView.trailingAnchor, constant: -BorderWidth),
-               popupButton.bottomAnchor.constraint(equalTo: popupView.bottomAnchor, constant: -BorderWidth)
-               ])
+        
+        // Popup rounded corner
+        popupView.layer.cornerRadius = 20
+        
+        // Popup Background
+        popupView.backgroundColor = UIColor.white
+        popupView.layer.borderWidth = BorderWidth
+        popupView.layer.masksToBounds = true
+        popupView.layer.borderColor = UIColor.white.cgColor
+        
+        // Popup Title
+        popupTitle.textColor = UIColor.white
+        popupTitle.backgroundColor = #colorLiteral(red: 0.3382441103, green: 0.7381854653, blue: 0, alpha: 1)
+        popupTitle.layer.masksToBounds = true
+        popupTitle.adjustsFontSizeToFitWidth = true
+        popupTitle.clipsToBounds = true
+        popupTitle.font = UIFont(name: "Poppins", size: 23.0)
+        popupTitle.numberOfLines = 1
+        popupTitle.textAlignment = .center
+        
+        // Popup Text
+        popupText.textColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+        popupText.font = UIFont.systemFont(ofSize: 16.0, weight: .semibold)
+        popupText.numberOfLines = 0
+        popupText.textAlignment = .center
+        
+        // Popup Button
+        popupButton.setTitleColor(UIColor.white, for: .normal)
+        popupButton.titleLabel?.font = UIFont.systemFont(ofSize: 23.0, weight: .bold)
+        popupButton.backgroundColor = #colorLiteral(red: 0.3382441103, green: 0.7381854653, blue: 0, alpha: 1)
+        
+        popupView.addSubview(popupTitle)
+        popupView.addSubview(popupText)
+        popupView.addSubview(popupButton)
+        
+        // Add the popupView(box) in the PopUpWindowView (semi-transparent background)
+        addSubview(popupView)
+        
+        
+        // PopupView constraints
+        popupView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            popupView.widthAnchor.constraint(equalToConstant: 293),
+            popupView.centerYAnchor.constraint(equalTo: self.centerYAnchor),
+            popupView.centerXAnchor.constraint(equalTo: self.centerXAnchor)
+        ])
+        
+        // PopupTitle constraints
+        popupTitle.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            popupTitle.leadingAnchor.constraint(equalTo: popupView.leadingAnchor, constant: BorderWidth),
+            popupTitle.trailingAnchor.constraint(equalTo: popupView.trailingAnchor, constant: -BorderWidth),
+            popupTitle.topAnchor.constraint(equalTo: popupView.topAnchor, constant: BorderWidth),
+            popupTitle.heightAnchor.constraint(equalToConstant: 55)
+        ])
+        
+        
+        // PopupText constraints
+        popupText.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            popupText.heightAnchor.constraint(greaterThanOrEqualToConstant: 67),
+            popupText.topAnchor.constraint(equalTo: popupTitle.bottomAnchor, constant: 8),
+            popupText.leadingAnchor.constraint(equalTo: popupView.leadingAnchor, constant: 15),
+            popupText.trailingAnchor.constraint(equalTo: popupView.trailingAnchor, constant: -15),
+            popupText.bottomAnchor.constraint(equalTo: popupButton.topAnchor, constant: -8)
+        ])
+        
+        
+        // PopupButton constraints
+        popupButton.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            popupButton.heightAnchor.constraint(equalToConstant: 44),
+            popupButton.leadingAnchor.constraint(equalTo: popupView.leadingAnchor, constant: BorderWidth),
+            popupButton.trailingAnchor.constraint(equalTo: popupView.trailingAnchor, constant: -BorderWidth),
+            popupButton.bottomAnchor.constraint(equalTo: popupView.bottomAnchor, constant: -BorderWidth)
+        ])
     }
     
     required init?(coder: NSCoder) {
@@ -120,7 +123,7 @@ class PopUpWindow: UIViewController {
         popUpWindowView.popupButton.addTarget(self, action: #selector(dismissView), for: .touchUpInside)
         view = popUpWindowView
     }
-
+    
     required init(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
