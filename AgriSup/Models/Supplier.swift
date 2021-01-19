415e804ac15e5626a13ab1582d484880e4eebad1
Sonney Patel <sonney.patel@gmail.com>
2021-01-19 15:11:52 +0000

    Completed the SupplierBuilder Singleton Object

--- AgriSup/Models/Supplier.swift
+++ AgriSup/Models/Supplier.swift
@@ -10,56 +10,60 @@ import Foundation
 
 struct Supplier: Codable {
     
-    let id: String
-    let firstName: String
-    let lastName: String
-    let mobileNumber: String
-    let photo: [String]
-    let deliveryDates: [String] //enum?
-    let deliveryCities: [String]
+    let firstName: String?
+    let lastName: String?
+    let mobileNumber: String?
+    let photo: [String]?
+    let deliveryDates: [String]?//enum?
     let credentials: Credentials
-    let transportProvider: TransportProvider
-    let depotProvider: DepotProvider
-    let financialInfo: FinancialInfo
-    let Products: [Products]
+    let deliveryCities: [String]
+    let deliveryCityInfo: [DeliveryCityInfo]
+    let financialInfo: FinancialInfo?
+    let Products: [Products]?
     
     struct Credentials: Codable {
-        let abn: String
-        let farmingLicense: String
-        let drivingLicense: String
-    }
-    
-    struct TransportProvider: Codable {
-        let name: String
-        let businessName: String
-        let mobileNumber: String
-    }
-    
-    struct DepotProvider: Codable {
-        let businessName: String
-        let mobileNumber: String
-        let address: String
-        let postcode: String
+        let abn: String?
+        let farmingLicense: String?
+        let drivingLicense: String?
     }
     
     //Do we need to encrypt this information?
     struct FinancialInfo: Codable {
-        let accountName: String
-        let bsb: String
-        let accountNumber: String
+        let accountName: String?
+        let bsb: String?
+        let accountNumber: String?
     }
     
     struct Products: Codable {
-        let name: String
-        let unit: String
-        let minOrderQuantity: Int
-        let leadTime: Int
-        let leadTimeUnits: String //enum
-        let samples: Bool
+        let name: String?
+        let unit: String?
+        let minOrderQuantity: Int?
+        let leadTime: Int?
+        let leadTimeUnits: String? //enum
+        let samples: Bool?
         let sampleUnits: Int?
-        let unitPrice: Double
-        let currency: String //enum
+        let unitPrice: Double?
+        let currency: String? //enum
     }
     
     
+}
+
+struct DeliveryCityInfo: Codable {
+    
+    let cityName: String
+    let transportProvider: TransportProvider
+    let depotProvider: DepotProvider
+}
+
+struct TransportProvider: Codable {
+    let name: String
+    let businessName: String
+    let mobileNumber: String
+}
+
+struct DepotProvider: Codable {
+    let businessName: String
+    let mobileNumber: String
+    let address: String
 }
--- AgriSup/Models/SupplierBuilder.swift
+++ AgriSup/Models/SupplierBuilder.swift
@@ -6,3 +6,62 @@
 //
 
 import Foundation
+
+class SupplierBuilder {
+    
+    
+    //Use this instance only
+    static let builder = SupplierBuilder()
+    
+    //private initializer for singleton
+    private init() {}
+    
+    var email : String = ""
+    var firstName : String = ""
+    var lastName : String = ""
+    var mobileNumber : String = ""
+    var abn: String = ""
+    var farmingLicense: String = ""
+    var driversLicense: String = ""
+    var deliveryDates: [String] = []
+    var deliveryCityInfo: [DeliveryCityInfo] = []
+    
+    
+    func setEmail(email: String) -> SupplierBuilder {
+        self.email = email
+        return self
+    }
+    
+    func setIdentity(firstName: String, lastName:String, mobileNumber: String) -> SupplierBuilder{
+        self.firstName = firstName
+        self.lastName = lastName
+        self.mobileNumber = mobileNumber
+        return self
+    }
+    
+    func setCredentials(abn: String, farmingLicense: String, driversLicense: String) -> SupplierBuilder {
+        self.abn = abn
+        self.farmingLicense = farmingLicense
+        self.driversLicense = driversLicense
+        return self
+    }
+    
+    func setDeliveryDates(deliveryDates: [String]) -> SupplierBuilder {
+        self.deliveryDates = deliveryDates
+        return self
+    }
+    
+    func appendDeliveryCityInfo(cityName: String, driverName: String, driverBusinessName: String, driverMobile: String, depotName: String, depotPhoneNumber: String, depotAddress: String) -> SupplierBuilder {
+        let transportProvider: TransportProvider = TransportProvider(name: driverName, businessName: driverBusinessName, mobileNumber: driverMobile)
+        let depotProvider: DepotProvider = DepotProvider(businessName: depotName, mobileNumber: depotPhoneNumber, address: depotAddress)
+        
+        let deliveryCityInfo = DeliveryCityInfo(cityName: cityName, transportProvider: transportProvider, depotProvider: depotProvider)
+        
+        self.deliveryCityInfo.append(deliveryCityInfo)
+        
+        return self
+    }
+    
+    
+    
+}
